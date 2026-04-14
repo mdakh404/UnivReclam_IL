5227909d82fbd145c018df66b87ba8bdf497cfc7
mdakh404 <akhrazmoad14@gmail.com>
2026-04-14 20:58:02 +0100

    Adding header.php and footer.php to includes/ dir

---
+++ includes/header.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="UnivReclam est une plateforme de gestion des réclamations pour les universités, offrant une interface conviviale pour les étudiants et le personnel administratif afin de soumettre, suivre et résoudre efficacement les réclamations liées aux services universitaires.">
+    <meta name="author" content="AKHRAZ MOAAD, ABIB ABDELWAHAB, OUBELLA SABER">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+    <title>UnivReclam | Gestion des Réclamations</title>
+    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/header-styles.css" rel="stylesheet">
+  </head>
+  <body>
+  <header>
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark p-3">
+        <a class="navbar-brand" href="#">UnivReclam</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Page d'Accueil</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+
+    <main role="main" class="container">
+      <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+      <p class="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the <code>body &gt; .container</code>.</p>
+      <p>Back to <a href="../sticky-footer/">the default sticky footer</a> minus the navbar.</p>
+    </main>
+    
+    <?php include 'footer.php'; ?>
+    <!--<footer class="footer">
+      <div class="container">
+        <span class="text-muted">Place sticky footer content here.</span>
+      </div>
+    </footer>-->
+
+    <script src="../bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
